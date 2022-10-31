112cae25642b88d82708580380a47f580dd4d378
aidata <aidata@10.121.3.207>
2022-10-31 09:44:54 +0300

    ı

---
+++ arrayList/src/arrayList/arrayList.java
@@ -0,0 +1,33 @@
+package arrayList;
+
+import java.util.ArrayList;
+
+public class arrayList {
+
+	public static void main(String[] args) {
+		ArrayList<String>    item=new ArrayList<String>();
+		item.add("red");
+		item.add("yellow");
+		item.add(0,"green");
+		for(String renk:item){
+			
+			System.out.print(" " +renk);
+			
+			
+		}
+		item.remove("red");
+for(String renk:item){
+	System.out.print(" ");
+			System.out.print(" " +renk);
+			
+			
+		}
+System.out.println(" ");
+
+System.out.print(item.get(1));
+
+System.out.println(item.contains("red")? "evet var": "hayýr yok");
+	}
+	
+
+}
